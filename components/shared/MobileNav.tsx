52cf6f602ab719059d7b24fb67c58d68d9cb7910
PankajKDev <pankajk.dev0000@gmail.com>
2024-05-28 16:30:31 +0530

    css fixes

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -72,7 +72,7 @@ const MobileNav = () => {
                     );
                   })}
                 </ul>
-                <div className="flex flex-col flex-center mt-5">
+                <div className="flex flex-col flex-center mt-3">
                   <Link href="/privacy">
                     <p className="cursor-pointer text-purple-700 mr-2">
                       Privacy Policy
